ee0719f27656e473a94cd583cd9d1ca731046698
SteadyB <16727826+SteadyB@users.noreply.github.com>
2018-05-16 11:06:06 +0200

    init project

---
+++ README.md
@@ -0,0 +1,4 @@
+orleans-0218-regioncentre
+=========================
+
+A Symfony project created on May 16, 2018, 8:38 am.
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+    var Encore = require('@symfony/webpack-encore');
+
+    Encore
+        .setOutputPath('web/build/')
+        .setPublicPath('/web')
+        .addEntry('app', './assets/js/app.js')
+	.addEntry('style', './assets/scss/main.scss')
+        .cleanupOutputBeforeBuild()
+        .enableBuildNotifications()
+	.enableSassLoader();
+
+    module.exports = Encore.getWebpackConfig();
