dc185e869b26c105a9802bd06968f8a15ba4b817
SteadyB <16727826+SteadyB@users.noreply.github.com>
2018-06-01 10:22:54 +0200

    travis fix #2

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -17,43 +17,43 @@ class UserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-            $builder
-                ->add('roles',ChoiceType::class, [
-                    'label'=> 'Type d\'utilisateur',
-                    'choices' => [
-                        'Administrateur Region' => 'ROLE_SUPER_ADMIN',
-                        'EMOP' => 'ROLE_POWER_USER',
-                        'Lycée'  => 'ROLE_USER',
-                    ],
-                    'expanded' => true,
-                    'multiple' => true,
-                    'required' => true,
-                        ])
-                ->add('firstName', TextType::class, [
-                    'attr' => ['maxlength' => '255', 'minlength' => '2'],
-                    'label'=> 'Prénom',
-                    'required' => true,
-                        ])
-                ->add('lastName', TextType::class, [
-                    'attr' => ['maxlength' => '255', 'minlength' => '2'],
-                    'label'=> 'Nom de famille',
-                    'required' => true,
-                ])
-                ->add('work', TextType::class, [
-                    'attr' => ['maxlength' => '255', 'minlength' => '2' ],
-                    'label'=> 'Fonction / Poste',
-                    'required' => true,
-                ])
-                ->add('phoneNumber',TelType::class, [
-                    'attr' => ['maxlength' => '30', 'minlength'=> '10'],
-                    'label'=> 'Numéro de téléphone',
-                    'required' => true,
-                ])
-                ->add('mail', EmailType::class, [
-                    'attr' => ['maxlength' => '255'],
-                    'label'=>'Couriel',
-                    'required' => true,
-                ]);
+        $builder
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Type d\'utilisateur',
+                'choices' => [
+                    'Administrateur Region' => 'ROLE_SUPER_ADMIN',
+                    'EMOP' => 'ROLE_POWER_USER',
+                    'Lycée' => 'ROLE_USER',
+                ],
+                'expanded' => true,
+                'multiple' => true,
+                'required' => true,
+            ])
+            ->add('firstName', TextType::class, [
+                'attr' => ['maxlength' => '255', 'minlength' => '2'],
+                'label' => 'Prénom',
+                'required' => true,
+            ])
+            ->add('lastName', TextType::class, [
+                'attr' => ['maxlength' => '255', 'minlength' => '2'],
+                'label' => 'Nom de famille',
+                'required' => true,
+            ])
+            ->add('work', TextType::class, [
+                'attr' => ['maxlength' => '255', 'minlength' => '2'],
+                'label' => 'Fonction / Poste',
+                'required' => true,
+            ])
+            ->add('phoneNumber', TelType::class, [
+                'attr' => ['maxlength' => '30', 'minlength' => '10'],
+                'label' => 'Numéro de téléphone',
+                'required' => true,
+            ])
+            ->add('mail', EmailType::class, [
+                'attr' => ['maxlength' => '255'],
+                'label' => 'Couriel',
+                'required' => true,
+            ]);
     }
 
     /**
