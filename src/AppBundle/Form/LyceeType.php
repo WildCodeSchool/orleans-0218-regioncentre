3a833191c37c431e0df697a75111e1b4da3c6b6b
maxime goyard <maxime.goyard@live.fr>
2018-06-04 08:08:12 +0200

    correction IntegerType Lycee

--- src/AppBundle/Form/LyceeType.php
+++ src/AppBundle/Form/LyceeType.php
@@ -2,11 +2,11 @@
 
 namespace AppBundle\Form;
 
-use Doctrine\DBAL\Types\IntegerType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class LyceeType extends AbstractType
 {
