e3180fdfd7428eeea52d9e5a8b2a5fe85edd8967
Sébastien <sebstn.hkzt@laposte.net>
2018-06-05 16:08:01 +0200

    S4_US08_#02 Création de la route vers la page de gestion

---
+++ src/AppBundle/Controller/ManagementController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Class ManagementController
+ * @package AppBundle\Controller
+ * @Route("gestion")
+ */
+class ManagementController extends Controller
+{
+    /**
+     * Management page
+     * Listing of works.
+     *
+     * @Route("/", name="admin_management")
+     * @Method("GET")
+     */
+    public function managementAction()
+    {
+        return $this->render('admin/management.html.twig');
+    }
+
+}
