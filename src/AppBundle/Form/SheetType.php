7134ac56e0eb190be440ef16863964b89ad0729a
Raphael Mouton <raphaelmouton@live.fr>
2018-06-06 14:46:34 +0200

    Ajout userID dans sheet

--- src/AppBundle/Form/SheetType.php
+++ src/AppBundle/Form/SheetType.php
@@ -3,9 +3,7 @@
 namespace AppBundle\Form;
 
 use AppBundle\Entity\Metier;
-use AppBundle\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
